3c8cbd815ad40f1a85352b9fb1453bd12b294fd9
DykiGH <arditi.dylan@gmail.com>
2024-06-21 11:21:50 +0200

    assert produit test

---
+++ src/Form/ArticleFormType.php
@@ -0,0 +1,30 @@
+<?php
+//! pê à DEL
+//TODO ajouter liens bdd OneToMany afin d'avoir un form pour ajouter entièrement un article
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_prod')
+            ->add('description_prod')
+            ->add('prix_prod')
+            ->add('stock')
+            ->add('disponibilite')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
