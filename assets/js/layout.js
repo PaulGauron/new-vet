9485e12fda4584df026dbe1de89c5436e352107e
DykiGH <arditi.dylan@gmail.com>
2024-08-13 17:42:11 +0200

    preparation queries produit

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,3 +5,7 @@ const nav = document.querySelector('nav');
 burgerMenu.addEventListener('click', () => {
     nav.classList.toggle('hidden');
 });
+
+const sidePanel = document.getElementById('side-panel');
+
+sidePanel.addEventListener()
